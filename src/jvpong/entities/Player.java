a13b762113ff2095e7e85fb567eede8d32d61204
lucasllf2608 <lucas.llf2608@gmail.com>
2025-07-06 16:52:13 -0300

    collision system

--- src/jvpong/entities/Player.java
+++ src/jvpong/entities/Player.java
@@ -3,16 +3,22 @@ package jvpong.entities;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import jvpong.Game;
+
 public class Player {
 	
 	public boolean right;
 	public boolean left;
 	public int x;
 	public int y;
+	public int width = 40;
+	public int height;
 
 	public Player(int x, int y) {
 		this.x = x;
 		this.y = y;
+		this.width = 40;
+		this.height = 10;
 	}
 	
 	
@@ -22,6 +28,14 @@ public class Player {
 		} else if (left) {
 			x--;
 		}
+		
+		
+		if(x+width > Game.WIDTH) {
+			x = Game.WIDTH - width;
+		} else if ( x < 0 ) {
+			x = 0;
+		}
+		
 	}
 	
 	public void render(Graphics g) {
